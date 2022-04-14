fa713ae63e86c5b5aa45b89287d884c023f6d420
Tim Jenness <tjenness@lsst.org>
2022-04-14 16:27:18 -0700

    Add a special test git repository
    Added as a tar file that is extracted before the tests run.

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -10,6 +10,8 @@
 # license that can be found in the LICENSE file.
 
 import os
+import sys
+import tarfile
 import unittest
 
 try:
@@ -20,7 +22,19 @@ except ImportError:
 from lsst_versions import find_dev_lsst_version
 
 TESTDIR = os.path.abspath(os.path.dirname(__file__))
-GITDIR = os.path.normpath(os.path.join(TESTDIR, os.pardir))
+GITDIR = os.path.join(TESTDIR, "repo")
+TARFILE = os.path.join(TESTDIR, "test-repo.tgz")
+
+
+def setup_module(module):
+    """Ensure that the test git repository is present.
+
+    This repository is stored as a tar file and must be unpacked
+    before the tests run.
+    """
+    if not os.path.exists(GITDIR):
+        with tarfile.open(TARFILE, "r:gz") as tar:
+            tar.extractall(path=TESTDIR)
 
 
 @unittest.skipIf(git is None, "GitPython package is not installed.")
@@ -34,10 +48,17 @@ class VersionsTestCase(unittest.TestCase):
             raise unittest.SkipTest("Git repository for this package is not accessible.")
 
     def test_versions(self):
-        """Determine versions of this package."""
+        """Determine versions of a test repository."""
         versions = (
-            # Effectively a no-op until the package has some tags.
-            ("53d5d17", "1.0.0a00000000"),
+            ("86b5d01", "1.0.0a00000001"),  # v1.0
+            ("ea28756", "2.0.0a20220400"),
+            ("af0c308", "2.0.0a20220100"),
+            ("w.2022.1", "2.0.0a20220100"),
+            ("da7a09d", "2.0.0a20220401"),
+            ("v2.1.0", "2.0.0a20220900"),  # Should be 2.1.0
+            ("w.2022.05", "2.0.0a20220600"),
+            ("v3.0.0", "3.0.0a20220903"),
+            ("3082cf0", "4.0.0a20221001"),
         )
 
         for tag, expected in versions:
@@ -47,4 +68,5 @@ class VersionsTestCase(unittest.TestCase):
 
 
 if __name__ == "__main__":
+    setup_module(sys.modules[__name__])
     unittest.main()
